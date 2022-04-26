84f0ec971327f8159652c3b69897399fda14cb3f
Diego Pérez Ortega <65054637+diegoperezo97@users.noreply.github.com>
2022-04-25 20:18:35 -0500

    Map GeoJSON polygons.

--- Module 13/map-geojson-polygons/static/js/logic.js
+++ Module 13/map-geojson-polygons/static/js/logic.js
@@ -9,7 +9,7 @@ let streets = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
 });
 
 // We create the dark view tile layer that will be an option for our map:
-let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -17,29 +17,35 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Create a base layer that holds both maps:
 let baseMaps = {
-    Street: streets,
-    Dark: dark
+    "Street": streets,
+    "Satellite Streets": satelliteStreets
 }
 
 // Create the map object with center, zoom level and default layer:
 let map = L.map("mapid", {
-    center: [30, 30],
+    center: [43.7, -79.3],
     zoom: 2,
-    layers: [dark]
+    layers: [streets]
 })
 
 // Pass our map layers into our layers control and add the layers control to the map:
 L.control.layers(baseMaps).addTo(map);
 
-// Accessing the airport GeoJSON URL:
-let torontoHoods = "https://raw.githubusercontent.com/diegoperezo97/mapping-earthquakes/main/Module%2013/torontoNeighborhoods.json"
+// Accessing the Toronto neighborhoods GeoJSON URL.
+let torontoHoods = "https://raw.githubusercontent.com/<GitHub_name>/Mapping_Earthquakes/main/torontoNeighborhoods.json";
+
+// Create a style for the lines:
+let myStyle = {
+    color: "#ffffa1",
+    weight: 1
+}
 
 // Grabbing our GeoJSON data:
-d3.json(torontoHoods).then(function (data) {
+d3.json(airportData).then(function (data) {
     console.log(data);
     // Creating a GeoJSON layer with the retrieved data:
     L.geoJSON(data, {
-        // We turn each feature into a market on the map:
+        myStyle,
         pointToLayer: function (feature, coordinates) {
             console.log(feature);
             return L.marker(coordinates)
@@ -47,4 +53,3 @@ d3.json(torontoHoods).then(function (data) {
         }
     }).addTo(map);
 });
-
