e53093965f80eabf9779f965c80cc9f486f048b3
Diego Pérez Ortega <65054637+diegoperezo97@users.noreply.github.com>
2022-06-13 13:53:23 -0500

    Module 13 finished.

--- Challenge 13/static/js/challenge_logic.js
+++ Challenge 13/static/js/challenge_logic.js
@@ -32,12 +32,14 @@ let baseMaps = {
 let allEarthquakes = new L.LayerGroup();
 let tectonicPlates = new L.LayerGroup();
 let majorEarthquakes = new L.LayerGroup();
+let japanEarthquakes = new L.LayerGroup();
 
 // 2. Add a reference to the major earthquake group to the overlays object.
 let overlays = {
   "Earthquakes": allEarthquakes,
   "Tectonic Plates": tectonicPlates,
-  "Major Earthquakes": majorEarthquakes
+  "Major Earthquakes": majorEarthquakes,
+  "Japan Earthquakes": japanEarthquakes
 };
 
 // Then we add a control to the map that will allow the user to change which
@@ -212,3 +214,88 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
     tectonicPlates.addTo(map);
   });
 });
+// ----------------------------------------------------------------------------------------------------------------------
+// Retrieve the earthquake GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function (data) {
+
+  const latNorth = 45.55744654393649;
+  const lngNorth = 148.75306873053898;
+  const latWest = 24.449434601264414;
+  const lngWest = 122.93364328614228;
+  const latSouth = 20.42528655292881; 
+  const lngSouth = 136.0697299383394;
+  const latEast = 24.28306426965769;
+  const lngEast = 153.98639629650194;
+
+  var earthquakesInJapan = data.filter(function(earthquake) {
+    if {
+      earthquake.features[0].geometry.coordinates[0] =< latNorth &&
+      earthquake.features[0].geometry.coordinates[0] =< latEast &&
+      earthquake.features[0].geometry.coordinates[0] >= latWest &&
+      earthquake.features[0].geometry.coordinates[0] >= latSouth &&
+    }
+
+  })
+
+  // This function returns the style data for each of the earthquakes we plot on
+  // the map. We pass the magnitude of the earthquake into two separate functions
+  // to calculate the color and radius.
+  function styleInfo(feature) {
+    return {
+      opacity: 1,
+      fillOpacity: 1,
+      fillColor: getColor(feature.properties.mag),
+      color: "#000000",
+      radius: getRadius(feature.properties.mag),
+      stroke: true,
+      weight: 0.5
+    };
+  }
+
+  // This function determines the color of the marker based on the magnitude of the earthquake.
+  function getColor(magnitude) {
+    if (magnitude > 5) {
+      return "#ea2c2c";
+    }
+    if (magnitude > 4) {
+      return "#ea822c";
+    }
+    if (magnitude > 3) {
+      return "#ee9c00";
+    }
+    if (magnitude > 2) {
+      return "#eecc00";
+    }
+    if (magnitude > 1) {
+      return "#d4ee00";
+    }
+    return "#98ee00";
+  }
+
+  // This function determines the radius of the earthquake marker based on its magnitude.
+  // Earthquakes with a magnitude of 0 were being plotted with the wrong radius.
+  function getRadius(magnitude) {
+    if (magnitude === 0) {
+      return 1;
+    }
+    return magnitude * 4;
+  }
+
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJson(data, {
+    // We turn each feature into a circleMarker on the map.
+    pointToLayer: function (feature, latlng) {
+      console.log(data);
+      return L.circleMarker(latlng);
+    },
+    // We set the style for each circleMarker using our styleInfo function.
+    style: styleInfo,
+    // We create a popup for each circleMarker to display the magnitude and location of the earthquake
+    //  after the marker has been created and styled.
+    onEachFeature: function (feature, layer) {
+      layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+    }
+  }).addTo(allEarthquakes);
+
+  // Then we add the earthquake layer to our map.
+  allEarthquakes.addTo(map);
--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# Mapping Earthquakes with JS & APIs
+# MODULE 13: Mapping Earthquakes with JS & APIs
+
+## Overview of the Analysis
+
+* I have just secured a fantastic job opportunity: _"Specialist in Data Visualization for the Disaster Reporting Network"_. A non-profit organization that uses statistics to create stories about disasters all across the world. If I accept the mission, I will be tasked with creating interesting data visualizations with interactive elements on earthquakes from across the world.
+
+* Basil, the chief of the seismic disaster response team, feels that this initiative will be beneficial to his team of reporters and data visualization experts. He expects that producing earthquake maps that are both informative and easy to use on desktops and mobile phones would help the Disaster Reporting Network gain traction.
+
+* In my new role, I'll assist in the construction of websites and mobile applications by utilizing the most recent earthquake GeoJSON data from the USGS website. Using JavaScript and the D3 and leaflet libraries, I'll browse and obtain earthquake data, then plot it on a Mapbox map via an API call.
+
+* Each earthquake's magnitude and location will be displayed in a popup marker on your map. The size and color of the marks for each earthquake should correspond to the magnitude of the event. Higher-magnitude earthquakes will look larger and darker in color, and a legend will provide context for my map data.
+
+* Finally, fault lines will be added to the map to show the link between the location and frequency of seismic activity and tectonic plates. I'll be able to upload my earthquake map to your online portfolio once I've finished it. If I play my cards well, the team may invite me to continue on and build similar disaster maps in the future.
+
+## Results of the Analysis
+
+**Image 1. Final Mapbox Map with Major Earthquakes over Satelite Layer**
+
+![Captura de Pantalla 2022-04-28 a la(s) 15 11 58](https://user-images.githubusercontent.com/65054637/165841081-6586c106-a49f-4fb3-9f97-e89f16cfdcfe.png)
+
+**Image 2. Final Mapbox Map with Tectonic Plates over Satelite Layer**
+
+![Captura de Pantalla 2022-04-28 a la(s) 15 12 09](https://user-images.githubusercontent.com/65054637/165841089-910c8d2e-4160-442a-9f51-56bbc94efb44.png)
+
+**Image 3. Final Mapbox Map with All Earthquakes and Tectonic Plates over Street Layer**
+
+![Captura de Pantalla 2022-04-28 a la(s) 15 12 27](https://user-images.githubusercontent.com/65054637/165841099-61a7deac-7ae6-408b-83b9-627954b32950.png)
